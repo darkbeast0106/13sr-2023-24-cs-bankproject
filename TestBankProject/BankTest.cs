d58c9c1f1d3c61e46f55328928c701064779395b
Budaházi Bence <budahazi.bence@petrik.hu>
2024-03-25 15:54:56 +0100

    Első teszteset hosszáadása

---
+++ TestBankProject/BankTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestBankProject
+{
+    internal class BankTest
+    {
+        [Test]
+        public void UjSzamlaEgyenleg0()
+        {
+            Bank b = new Bank();
+            b.UjSzamla("Gipsz Jakab", "1234");
+            Assert.That(b.Egyenleg("1234"), Is.Zero);
+        }
+    }
+}
---
+++ TestBankProject/UnitTest1.cs
@@ -0,0 +1,28 @@
+//namespace TestBankProject
+//{
+//    public class Tests
+//    {
+//        [SetUp]
+//        public void Setup()
+//        {
+//        }
+
+//        [Test]
+//        public void Test1()
+//        {
+//            Assert.Pass();
+//        }
+
+//        [Test] 
+//        // Test annotáció - ez jelöli, hogy az adott függvény egy teszteset
+//        public void Test2()
+//        {
+//            Assert.Fail();
+//        }
+
+//        public void Test3()
+//        {
+//            Assert.Pass();
+//        }
+//    }
+//}
